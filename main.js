d0424de191403eda37861b164e7240397b54a6cc
jack23w <113193681+jack23w@users.noreply.github.com>
2023-10-12 15:07:59 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+//console.log("hello dilan");
+//alert("netnot");
+//prompt("puh sepuh");
+//var Promnet ="life not enough"
+//console.log(Promnet)
+//Promnet ="liee"
+//console.log(Promnet)
+
+//let totalpoints = prompt ("x = 6")
+//if (totalpoints>100)
+//{
+    //document.write("horeee")
+//}
+//else
+//{
+    //document.write("yahhh")
+//}
+
+    //let x = 6;
+    //let y = 3;
+
+    //var a = (x < 10 && y > 1);
+    //var b = (x < 10 && y < 1);
+    //var c = (x == 5 || y == 5); 
+    //var d = (x == 6 || y == 5); 
+    //var e = !(x == y);
+
+    //document.write("<br>x = 6; y = 3</br>");
+    //document.write("<br>a) x < 10 && y > 1;</br>" + a);
+    //document.write("<br>b)x < 10 && y < 1;</br>" + b);
+    //document.write("<br>c)x == 5 || y == 5;</br>" + c);
+    //document.write("<br>d)x == 6 || y == 5;</br>" + d);
+    //document.write("<br>e)x == y;</br>" + e);
+
+//console.log("a: " + a);
+//console.log("b: " + b);
+//console.log("c: " + c);
+//console.log("d: " + d); 
+//console.log("e: " + e); 
+
+//let p = document.querySelector("p");
+//let button = document.querySelector("button");
+//let input = document.querySelector("input");
+
+//button.addEventListener('click', function() {
+    //let isi = input.value;
+    //console.log(isi);
+    //p.innerText = isi;
+//});
